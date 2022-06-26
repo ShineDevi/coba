0d401b563b7d486d7281a3c17d3a5e2c258ccb39
ShineDevi <shinedevi02@gmail.com>
2022-06-26 16:04:12 +0700

    first commit

---
+++ database/migrations/2022_06_26_085630_create_kendaraan_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKendaraanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kendaraan', function (Blueprint $table) {
+            $table->id('id_kendaraan');
+            $table->integer('id_jenis_kendaraan');
+            $table->string('plat_kendaraan', 20);
+            $table->string('foto_kendaraan', 20);
+            $table->string('status_kendaraan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kendaraan');
+    }
+}
